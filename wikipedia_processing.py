49908f0be1ca09abe161b5fecaa327e9fa232d31
C_Walls <calebmarcuswalls@gmail.com>
2024-08-04 11:59:14 -0500

    Refactor batch processing

--- wikipedia_processing.py
+++ wikipedia_processing.py
@@ -11,43 +11,44 @@ def load_wikipedia_dataset(wikiDS_path):
     if not os.path.exists(wikiDS_path):
         wiki = load_dataset("wikimedia/wikipedia", "20231101.en")
         wiki.save_to_disk(wikiDS_path)
-        print(f"Downloaded and saved Wikipedia dataset to {wikiDS_path}")
+        print(f"Downloaded and saved Wikipedia dataset to '{wikiDS_path}'")
     else:
         wiki = load_from_disk(wikiDS_path)
-        print(f"Loaded Wikipedia dataset from {wikiDS_path}")
+        print(f"Loaded Wikipedia dataset from '{wikiDS_path}'")
     return wiki
 
-def process_articles(corpus_path, wikiDS_path):
+def build_wiki_corpus(corpus_path, wikiDS_path):
     wiki = load_wikipedia_dataset(wikiDS_path)
-    corpus = []
     translator = str.maketrans('', '', string.punctuation + string.whitespace)
     article_count = len(wiki['train'])
+    chunk_size = 1000
 
-    for article in tqdm(wiki['train'], total=article_count, desc="Processing articles", unit="article"):
-        words = article['text'].split()
-        processed_words = [word.lower().translate(translator) for word in words if word.isalpha()]
-        corpus.extend(processed_words)
-
+    print(f"\nProcessing {article_count} articles from Wikipedia...")
     with open(corpus_path, "wb") as file:
-        pickle.dump(corpus, file)
-
-    print(f"Saved processed corpus to {corpus_path}") 
+        for i in tqdm(range(0, article_count, chunk_size), desc="Processing articles", unit="chunk"):
+            corpus = []
+            articles = wiki['train'].select(range(i, min(i + chunk_size, article_count)))
+            for article in articles:
+                words = article['text'].split()
+                processed_words = [word.lower().translate(translator) for word in words if word.isalpha()]
+                corpus.extend(processed_words)
+            
+            pickle.dump(corpus, file)
 
 def main():
     if os.path.exists(corpus_path) and os.path.exists(wikiDS_path):
-        print(f"\nWikipedia corpus and dataset already exist:\n{wikiDS_path}\n{corpus_path}")
+        print(f"\nWikipedia corpus and dataset already exist:\n'{wikiDS_path}'\n'{corpus_path}'")
         exit(0)
     elif os.path.exists(corpus_path):
-        print(f"\nProcessed corpus exists as a .pkl file in {corpus_path}.")
-        user_input = input("Would you like to also download the full unprocessed dataset? [Y/n]: ").strip().lower()
+        print(f"\nProcessed corpus exists as a .pkl file in '{corpus_path}'.")
+        user_input = input("\nWould you like to also download the full unprocessed dataset? [Y/n]: ").strip().lower()
         if user_input == 'y' or user_input == '':
             wiki = load_wikipedia_dataset(wikiDS_path)
-            print(f"Loaded Wikipedia dataset from {wikiDS_path}")
         else:
-            print("Download Skipped.")
+            print("\nDownload Skipped\n.")
             exit(0)
     else:
-        process_articles(corpus_path, wikiDS_path)
+        build_wiki_corpus(corpus_path, wikiDS_path)
 
 if __name__ == "__main__":
     main()
